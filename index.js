266d9df5001e93ffcb4b7df70401dc5404776f8c
NikolayK <nikolay.kim@codeimmersive.com>
2020-02-14 10:06:06 -0500

    finishes "movies" part

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const axios = require('axios')
+const morgan = require('morgan')
+const path = require('path')
+
+require('dotenv').config()
+const port = process.env.PORT || 8000
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(__dirname + '/public'))
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+
+app.get('/random', (req,res) => {
+   const url = 'https://randomuser.me/api/?results=20'
+})
+
+app.get('/movies', (req,res) => {
+   const url = 'https://api.themoviedb.org/3/movie/now_playing?api_key='
+   const key = process.env.API_KEY
+   const nowPlaying = '&language=en-US&page=1'
+   
+   const nowPlayingMovies = async()=> {
+      try {
+         const response = await axios.get(url+key+nowPlaying)
+         const data = response.data.results
+         return res.render('main/movies', {data})
+      } catch (error) {
+         console.error(error);
+      }
+   }
+   nowPlayingMovies()
+})
+
+app.listen(port, () => {
+   console.log(`Listening on port ${port}`)
+})
